f64b3dad45f1015a9b335605ee440504fc9ca882
nitesh-kumbalkar <nitesh.kumbalkar@gmail.com>
2024-07-06 23:30:45 +0530

    Create Node.java

---
+++ Node.java
@@ -0,0 +1,12 @@
+class Node {
+    private int key;
+    private int value;
+    private Node pre;
+    private Node next;
+ 
+    public Node(int key, int value)
+    {
+        this.key = key;
+        this.value = value;
+    }
+}
