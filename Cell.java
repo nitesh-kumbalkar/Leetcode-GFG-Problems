af2a86d5f8e4360a561c518717d897e9c40e580c
nitesh-kumbalkar <nitesh.kumbalkar@gmail.com>
2024-07-07 15:04:45 +0530

    Create Cell.java

---
+++ Cell.java
@@ -0,0 +1,10 @@
+class Cell {
+        int x, y;
+        int dis;
+        
+        public Cell(int x, int y, int dis) {
+            this.x = x;
+            this.y = y;
+            this.dis = dis;
+        }
+    }
